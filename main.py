81416ca17b1fec0901637131e76874830c329919
Narresh Gudimetlaa <venkatdada369@gmail.com>
2025-05-29 07:48:05 +0530

    Update main.py 123

--- main.py
+++ main.py
@@ -1,48 +1,9 @@
-# main.py
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
-import os
-
-# Import actual screen implementations
-from screens.register_screen import RegisterScreen
-from screens.tracker_screen import TrackerScreen
-
-
-class MainScreen(Screen):
-    def go_to_register(self, instance):
-        self.manager.current = "register"
-
-    def go_to_tracker(self, instance):
-        self.manager.current = "tracker"
-
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(Button(text="Go to Register",
-                                 on_press=self.go_to_register))
-        layout.add_widget(Button(text="Go to Tracker",
-                                 on_press=self.go_to_tracker))
-        self.add_widget(layout)
-
+from kivy.uix.label import Label
 
 class MyApp(App):
-
-
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(MainScreen(name='main'))
-        sm.add_widget(RegisterScreen(name='register'))
-
-        # Wrap TrackerScreen (a layout) inside a Screen
-        tracker_screen = Screen(name='tracker')
-        tracker_screen.add_widget(TrackerScreen())
-        sm.add_widget(tracker_screen)
-
-        return sm
-
-
+        return Label(text='Hello, Kivy!')
 
 if __name__ == '__main__':
     MyApp().run()
